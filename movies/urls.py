4986aefb4df4a606db35180143085ec5a87e1c68
ah9mon <eomgyeol97@gmail.com>
2023-04-28 12:24:20 +0900

    user follow, like and movie index, detail

--- movies/urls.py
+++ movies/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'movies'
+app_name = "movies"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:movie_pk>/', views.detail, name='detail'),
-    path('recommended/', views.recommended, name='recommended'),
+    path("", views.index, name="index"),
+    path("<int:movie_pk>/", views.detail, name="detail"),
+    path("recommended/<int:genre_pk>", views.recommended, name="recommended"),
 ]
