90557564aa65d2fa61a55c77a6cae33abc327462
hun23 <hunn2023@gmail.com>
2023-04-28 13:30:50 +0900

    recommend

--- community/forms.py
+++ community/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 from .models import Review, Comment
-
+from movies.models import Movie
 
 class ReviewForm(forms.ModelForm):
-    
+    choices = list(map(lambda x: (x.title, x.title), Movie.objects.all()))
+    movie_title = forms.ChoiceField(choices=choices)
     class Meta:
         model = Review
         fields = ['title', 'movie_title', 'rank', 'content']
